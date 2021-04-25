23920de1a0ea4fb5c85a7f71ba9a81c152c29c3e
Anthony Pernia <anthonyperniah@gmail.com>
2021-04-24 21:12:32 -0300

    documentacion

--- src/main/java/com/market/project/MarketApplication.java
+++ src/main/java/com/market/project/MarketApplication.java
@@ -3,11 +3,22 @@ package com.market.project;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * <h1>API Market</h1>
+ * Api market es una api que expone productos de una base de datos PostgreSQL
+ * Permite, agregar y eliminarlos, con la regla de autenticacion.
+ * @author anthonypernia
+ * @version 1.0
+ * @since 2021
+ */
+
+
 @SpringBootApplication
 public class MarketApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MarketApplication.class, args);
+
 	}
 
 }
